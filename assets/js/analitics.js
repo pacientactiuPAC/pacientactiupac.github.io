aa4fb0948350341142107e79e8fca048f53828ad
pacientactiuPAC <pacientactiu.apps@gmail.com>
2026-05-07 18:43:14 +0200

    s'inclou analitics a tots els moduls!

---
+++ assets/js/analitics.js
@@ -0,0 +1,44 @@
+// assets/js/analytics.js
+const GA_ID = "G-KSJEFLT3ZZ";
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+
+(function loadGoogleAnalytics() {
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+  document.head.appendChild(script);
+
+  gtag("js", new Date());
+  gtag("config", GA_ID, {
+    anonymize_ip: true
+  });
+})();
+
+function trackManualModule(moduleName) {
+  gtag("event", "manual_module_view", {
+    module_name: moduleName,
+    page_location: window.location.href,
+    page_title: document.title
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("[data-ga-module]").forEach((link) => {
+    link.addEventListener("click", () => {
+      gtag("event", "manual_module_click", {
+        module_name: link.dataset.gaModule,
+        link_url: link.href
+      });
+    });
+  });
+});
+
+function trackAppModule(moduleName) {
+  gtag("event", "app_module_view", {
+    module_name: moduleName,
+    page_location: window.location.href,
+    page_title: document.title
+  });
+}
